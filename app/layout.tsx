9a1816f1438d812d1cf08e17493fc03116fa062e
chikedav <115415500+chikedav@users.noreply.github.com>
2025-07-11 08:33:22 -0400

    change layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+// app/layout.tsx
 import { Metadata } from 'next';
 import Footer from '@/components/ui/Footer';
 import Navbar from '@/components/ui/Navbar';
@@ -11,22 +12,22 @@ const description = 'Automate Your Content and Business Growth with AI';
 
 export const metadata: Metadata = {
   metadataBase: new URL(getURL()),
-  title: title,
-  description: description,
+  title,
+  description,
   openGraph: {
-    title: title,
-    description: description
-  }
+    title,
+    description,
+  },
 };
 
 export default async function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en">
-      <body className="bg-black">
+    <html lang="en" className="scroll-smooth antialiased">
+      <body className="bg-white text-black dark:bg-zinc-900 dark:text-white transition-colors duration-300">
         <Navbar />
         <main
           id="skip"
-          className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
+          className="min-h-[calc(100dvh-4rem)] md:min-h-[calc(100dvh-5rem)]"
         >
           {children}
         </main>
